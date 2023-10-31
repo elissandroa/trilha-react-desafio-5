6cc8508e63d4a75f76f745865dc04ce7f276d419
elissandroa <elissandroa@hotmail.com>
2023-10-31 20:57:51 -0300

    Protect Api Keys

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api  = axios.create({
     baseURL: 'https://suxodusjhnidzojbvjsj.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1eG9kdXNqaG5pZHpvamJ2anNqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODY4Mjc5MCwiZXhwIjoyMDE0MjU4NzkwfQ.AVASxHoAFF0liq4sQ3P7gZGxqZOfXsaMsp7ckSxq76k",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1eG9kdXNqaG5pZHpvamJ2anNqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODY4Mjc5MCwiZXhwIjoyMDE0MjU4NzkwfQ.AVASxHoAFF0liq4sQ3P7gZGxqZOfXsaMsp7ckSxq76k"
+        apikey: "apikey",
+        authorization: "Authorization"
     }
 })
